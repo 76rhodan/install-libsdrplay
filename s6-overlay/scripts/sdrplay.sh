66aee0d11573d6bef7e0e780c40c7523757656c5
rhodan76 <53168891+76rhodan@users.noreply.github.com>
2024-01-18 10:06:55 +0100

    Restart on segfault

--- s6-overlay/scripts/sdrplay.sh
+++ s6-overlay/scripts/sdrplay.sh
@@ -12,3 +12,29 @@ s6wrap=(s6wrap --quiet --timestamps --prepend="$SCRIPT_NAME" --args)
 
 #shellcheck disable=SC2154
 "${s6wrap[@]}" /usr/bin/sdrplay_apiService
+
+# Define the string to monitor
+TARGET_STRING="sdrplay_apiServ\[[:digit:]]\+: segfault at"
+
+# File to store processed lines
+LOG_FILE="/tmp/processed_lines.log"
+
+# Create the log file if it doesn't exist
+touch "$LOG_FILE"
+
+# Monitor dmesg output continuously
+dmesg --follow | while read -r line
+do
+    # Check if the line matches the target pattern
+    if echo "$line" | grep -Eq "$TARGET_STRING"; then
+        # Check if the line has already been processed
+        if ! grep -Fxq "$line" "$LOG_FILE"; then
+            # Log the line to the processed lines file
+            echo "$line" >> "$LOG_FILE"
+
+            # Do sometthing
+            echo "Segfault found, restarting API"
+            s6-svc -r /var/run/sdrplay
+        fi
+    fi
+done
